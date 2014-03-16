e4576e8ff95fd5bd2f1501a4957cfcf4bdf4104c
David Herrmann <dh.herrmann@gmail.com>
2014-03-16 14:38:40 +0100

    BACKPORT: drm: enable render-nodes by default
    We introduced render-nodes about 1/2 year ago and no problems showed up.
    Remove the drm_rnodes argument and enable them by default now.
    
    Acked-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: David Herrmann <dh.herrmann@gmail.com>
    (cherry picked from commit 6d6dfcfb883818b40b58bac61cc72cab428a7a03)
    
    Change-Id: Ibbe493906fe9ee560b7bd1d6107912acad304985
    Reviewed-on: https://chromium-review.googlesource.com/241120
    Commit-Queue: Haixia Shi <hshi@chromium.org>
    Trybot-Ready: Haixia Shi <hshi@chromium.org>
    Tested-by: Haixia Shi <hshi@chromium.org>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
    (cherry picked from commit bf9d64d9b7b1a399451353c477e884780b8e2601)
    Reviewed-on: https://chromium-review.googlesource.com/241505
    Reviewed-by: Haixia Shi <hshi@chromium.org>

--- drivers/gpu/drm/drm_stub.c
+++ drivers/gpu/drm/drm_stub.c
@@ -40,9 +40,6 @@
 unsigned int drm_debug = 0;	/* 1 to enable debug output */
 EXPORT_SYMBOL(drm_debug);
 
-unsigned int drm_rnodes = 0;	/* 1 to enable experimental render nodes API */
-EXPORT_SYMBOL(drm_rnodes);
-
 unsigned int drm_vblank_offdelay = 5000;    /* Default to 5000 msecs. */
 EXPORT_SYMBOL(drm_vblank_offdelay);
 
@@ -53,12 +50,10 @@ MODULE_AUTHOR(CORE_AUTHOR);
 MODULE_DESCRIPTION(CORE_DESC);
 MODULE_LICENSE("GPL and additional rights");
 MODULE_PARM_DESC(debug, "Enable debug output");
-MODULE_PARM_DESC(rnodes, "Enable experimental render nodes API");
 MODULE_PARM_DESC(vblankoffdelay, "Delay until vblank irq auto-disable [msecs]");
 MODULE_PARM_DESC(timestamp_precision_usec, "Max. error on timestamps [usecs]");
 
 module_param_named(debug, drm_debug, int, 0600);
-module_param_named(rnodes, drm_rnodes, int, 0600);
 module_param_named(vblankoffdelay, drm_vblank_offdelay, int, 0600);
 module_param_named(timestamp_precision_usec, drm_timestamp_precision, int, 0600);
 
