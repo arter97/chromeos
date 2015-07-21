d8a81709cef0fd9534dbf5f75f55e2457660a511
Thierry Reding <treding@nvidia.com>
2015-07-21 16:33:48 +0200

    UPSTREAM: drm/dp: link: Track capabilities alongside settings
    Store capabilities in max_* fields and add separate fields for the
    currently selected settings.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>
    [from https://github.com/thierryreding/linux.git]
    (cherry picked from commit 282aadf46077d8801b3b0219b812a9c6d9fbda20)
    Signed-off-by: Sean Paul <seanpaul@chromium.org>
    
    Change-Id: Ic6c6e642abb129c31c3ff3dac067afc6bfdc8da0
    Original-Change-Id: I7e994e088af16891d8d4ccb7c37435f29edb7b4e
    Reviewed-on: https://chromium-review.googlesource.com/298323
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    Tested-by: Hung-Te Lin <hungte@chromium.org>

--- include/drm/drm_dp_helper.h
+++ include/drm/drm_dp_helper.h
@@ -824,8 +824,8 @@ struct drm_dp_link_ops {
 struct drm_dp_link {
 	unsigned char revision;
 	unsigned char edp;
-	unsigned int rate;
-	unsigned int num_lanes;
+	unsigned int max_lanes;
+	unsigned int max_rate;
 	unsigned long capabilities;
 	unsigned long aux_rd_interval;
 
@@ -833,6 +833,8 @@ struct drm_dp_link {
 	struct drm_dp_aux *aux;
 
 	struct drm_dp_link_train train;
+	unsigned int lanes;
+	unsigned int rate;
 };
 
 int drm_dp_link_probe(struct drm_dp_aux *aux, struct drm_dp_link *link);
