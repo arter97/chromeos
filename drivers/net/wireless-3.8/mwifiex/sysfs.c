abe237ac4974d4b2e7593900256a71ef3c86a244
Zhaoyang Liu <liuzy@marvell.com>
2015-07-21 01:05:14 -0700

    UPSTREAM: mwifiex: change dbg print func to mwifiex_dbg
    commit acebe8c10a6eabdb9c34370a774b5b3fcbae3ff4 upstream
    
    This patch changes all debug print functions from dev_dbg/dev_err/dev_info
    to mwifiex specific debug functions.
    
    Signed-off-by: Zhaoyang Liu <liuzy@marvell.com>
    Signed-off-by: Cathy Luo <cluo@marvell.com>
    Signed-off-by: Avinash Patil <patila@marvell.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    
    BUG=none
    TEST=With this patch series applied, verified that appropriate
    logs can be enabled by changing debug level. Also, ran basic scan,
    connect and data path tests.
    
    Reviewed-on: https://chromium-review.googlesource.com/276993
    Reviewed-by: Paul Stewart <pstew@chromium.org>
    
    Change-Id: Ib54f90d12fc4d09c90c44ce8e2104370832822e7
    Signed-off-by: Amitkumar Karwar <akarwar@marvell.com>
    Reviewed-on: https://chromium-review.googlesource.com/296758
    Reviewed-by: Dmitry Torokhov <dtor@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/305991

--- drivers/net/wireless-3.8/mwifiex/sysfs.c
+++ drivers/net/wireless-3.8/mwifiex/sysfs.c
@@ -80,14 +80,14 @@ int mwifiex_sysfs_register(struct mwifiex_private *priv)
 	/* Create sysfs file to control manufacturing mode feature*/
 	ret = device_create_file(&priv->netdev->dev, &dev_attr_mfg_mode);
 	if (ret)
-		dev_err(priv->adapter->dev,
-			"failed to create sysfs file mfg_mode\n");
+		mwifiex_dbg(priv->adapter, ERROR,
+			    "failed to create sysfs file mfg_mode\n");
 
 	/* Create sysfs file for manufacturing firmware name */
 	ret = device_create_file(&priv->netdev->dev, &dev_attr_mfg_firmware);
 	if (ret)
-		dev_err(priv->adapter->dev,
-			"failed to create sysfs file mfg_firmware\n");
+		mwifiex_dbg(priv->adapter, ERROR,
+			    "failed to create sysfs file mfg_firmware\n");
 
 	return ret;
 }
