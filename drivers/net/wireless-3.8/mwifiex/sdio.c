eabdd33bd597dd95460e2bc63d72dd630135c707
Xinming Hu <huxm@marvell.com>
2015-09-15 22:25:26 -0700

    CHROMIUM: mwifiex: minor corrections in multiport aggregation code
    This patch includes below changes
    1) Check if multiport aggregation feature flag while doing rx
    length validation check.
    2) Reset buffer size variables when buffers are freed.
    
    BUG=chrome-os-partner:43741, 44146, 43848
    TEST= Simulate 64k and/or 32k skb allocation faiures, confirmed
    that driver is successfully initialized after card reset.
    
    Change-Id: Ibf1fc4482cd9fd1ef483dc064a3cac400c54b574
    Signed-off-by: Xinming Hu <huxm@marvell.com>
    Signed-off-by: Amitkumar Karwar <akarwar@marvell.com>
    Reviewed-on: https://chromium-review.googlesource.com/300016
    Reviewed-by: Dmitry Torokhov <dtor@chromium.org>

--- drivers/net/wireless-3.8/mwifiex/sdio.c
+++ drivers/net/wireless-3.8/mwifiex/sdio.c
@@ -1531,8 +1531,9 @@ static int mwifiex_process_int_status(struct mwifiex_adapter *adapter)
 				(rx_len + MWIFIEX_SDIO_BLOCK_SIZE -
 				 1) / MWIFIEX_SDIO_BLOCK_SIZE;
 			if (rx_len <= INTF_HEADER_LEN ||
-			    (rx_blocks * MWIFIEX_SDIO_BLOCK_SIZE) >
-			     card->mpa_rx.buf_size) {
+			    (card->mpa_rx.enabled &&
+			    ((rx_blocks * MWIFIEX_SDIO_BLOCK_SIZE) >
+			     card->mpa_rx.buf_size))) {
 				dev_err(adapter->dev, "invalid rx_len=%d\n",
 					rx_len);
 				return -1;
@@ -1830,6 +1831,8 @@ error:
 	if (ret) {
 		kfree(card->mpa_tx.buf);
 		kfree(card->mpa_rx.buf);
+		card->mpa_tx.buf_size = 0;
+		card->mpa_rx.buf_size = 0;
 	}
 
 	return ret;
