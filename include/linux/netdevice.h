e1cc77edc5e2d762c7374656636da98a19838b98
Stephen Wang <wstephen@codeaurora.org>
2015-10-07 09:33:09 -0700

    CHROMIUM: net: Add netdevice notification for bridge activity
    This modification allows programs to get notified whenever a
    device is added to, or removed from a bridge. This will be used
    by NSS Qdisc for updating bridge shaper configuration.
    
    BUG=chrome-os-partner:42290
    TEST=build/boot on WhirlWind. Verified notification was recieved when bridge
    modification happens
    
    Change-Id: I0c32120dd3c39a0eb90a4deeb38c80fd036dbb44
    Signed-off-by: Stephen Wang <wstephen@codeaurora.org>
    Reviewed-on: https://chromium-review.googlesource.com/304357
    Commit-Ready: Kishan Kunduru <kkunduru@chromium.org>
    Tested-by: Kishan Kunduru <kkunduru@chromium.org>
    Reviewed-by: Kishan Kunduru <kkunduru@chromium.org>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -1,4 +1,20 @@
 /*
+ **************************************************************************
+ * Copyright (c) 2015, The Linux Foundation. All rights reserved.
+ * Permission to use, copy, modify, and/or distribute this software for
+ * any purpose with or without fee is hereby granted, provided that the
+ * above copyright notice and this permission notice appear in all copies.
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
+ * OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ **************************************************************************
+ */
+
+/*
  * INET		An implementation of the TCP/IP protocol suite for the LINUX
  *		operating system.  INET is implemented using the  BSD Socket
  *		interface as the means of communication with the user level.
@@ -1785,6 +1801,8 @@ struct pcpu_sw_netstats {
 #define NETDEV_CHANGEUPPER	0x0015
 #define NETDEV_RESEND_IGMP	0x0016
 #define NETDEV_PRECHANGEMTU	0x0017 /* notify before mtu change happened */
+#define NETDEV_BR_JOIN		0x0018
+#define NETDEV_BR_LEAVE		0x0019
 
 int register_netdevice_notifier(struct notifier_block *nb);
 int unregister_netdevice_notifier(struct notifier_block *nb);
--- net/bridge/br_if.c
+++ net/bridge/br_if.c
@@ -1,4 +1,20 @@
 /*
+ **************************************************************************
+ * Copyright (c) 2015, The Linux Foundation. All rights reserved.
+ * Permission to use, copy, modify, and/or distribute this software for
+ * any purpose with or without fee is hereby granted, provided that the
+ * above copyright notice and this permission notice appear in all copies.
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
+ * OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ **************************************************************************
+ */
+
+/*
  *	Userspace interface
  *	Linux ethernet bridge
  *
@@ -408,6 +424,7 @@ int br_add_if(struct net_bridge *br, struct net_device *dev)
 	dev_set_mtu(br->dev, br_min_mtu(br));
 
 	kobject_uevent(&p->kobj, KOBJ_ADD);
+	call_netdevice_notifiers(NETDEV_BR_JOIN, dev);
 
 	return 0;
 
@@ -442,6 +459,7 @@ int br_del_if(struct net_bridge *br, struct net_device *dev)
 	 * there still maybe an alternate path for netconsole to use;
 	 * therefore there is no reason for a NETDEV_RELEASE event.
 	 */
+	call_netdevice_notifiers(NETDEV_BR_LEAVE, dev);
 	del_nbp(p);
 
 	spin_lock_bh(&br->lock);
