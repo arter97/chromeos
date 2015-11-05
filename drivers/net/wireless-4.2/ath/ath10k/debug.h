rockchip-linux/kernel

CHROMIUM: Add debugfs interface to test some of smart antenna APIs
smart_ant_enable :
	To enable/disable smart antenna support. This configuration
should be the first one as soon as wifi interface is up and running.

echo 0|1 > /sys/kernel/debug/ieee80211/phyX/ath10k/smart_ant_enable

smart_ant_tx_ant:
	To slect a tx antenna combination for smart antenna for
a particular peer.

echo <peer_mac_separated by colon (:)> <antenna_combination> >
/sys/kernel/debug/ieee80211/phyX/ath10k/smart_ant_tx_ant

smart_ant_rx_ant:
	To select an antenna combination for Rx.

echo <antenna_combination> >
/sys/kernel/debug/ieee80211/phyX/ath10k/smart_ant_rx_ant

smart_ant_train_info:
	To trigger training on data packets.

echo <peer_mac> <ratecode> <antenna_combination> <num_pkts> >
/sys/kernel/debug/ieee80211/phyX/ath10k/smart_ant_train_info

Signed-off-by: Vasanthakumar Thiagarajan <vthiagar@codeaurora.org>

BUG=chrome-os-partner:37796
TEST=build/boot. Load ath10k_core with modparam set enable_smart_antenna=1,
bring up AP mode using hostapd. Enable smart antenna from debugs as mentioned
in the commit log and connect a station to this AP. Try configuring other
smart antenna debugfs interfaces and check the kernel log (debug_mask=0x4000)
for API effect. Device was able to connect to the internet and load multiple websites.
This testing is done on AP148.

Change-Id: I0288a8713829810cabe3facba4b8708c79623c2b
Reviewed-on: https://chromium-review.googlesource.com/254499
Tested-by: Peter Oh <poh@qca.qualcomm.com>
Reviewed-by: Toshi Kikuchi <toshik@chromium.org>
Commit-Queue: Toshi Kikuchi <toshik@chromium.org>

drivers/net/wireless-3.18/ath/ath10k/debug.h
@@ -36,6 +36,9 @@ enum ath10k_debug_mask {
 	ATH10K_DBG_REGULATORY	= 0x00000800,
 	ATH10K_DBG_TESTMODE	= 0x00001000,
 	ATH10K_DBG_WMI_PRINT	= 0x00002000,
+#ifdef CONFIG_ATH10K_SMART_ANTENNA
+	ATH10K_DBG_SMART_ANT	= 0x00004000,
+#endif
 	ATH10K_DBG_ANY		= 0xffffffff,
 };
 
@@ -84,6 +87,7 @@ int ath10k_debug_get_et_sset_count(struct ieee80211_hw *hw,
 void ath10k_debug_get_et_stats(struct ieee80211_hw *hw,
 			       struct ieee80211_vif *vif,
 			       struct ethtool_stats *stats, u64 *data);
+void ath10k_smart_ant_debugfs_init(struct ath10k *ar);
 #else
 static inline int ath10k_debug_start(struct ath10k *ar)
 {
@@ -133,6 +137,7 @@ ath10k_debug_get_new_fw_crash_data(struct ath10k *ar)
 #define ath10k_debug_get_et_strings NULL
 #define ath10k_debug_get_et_sset_count NULL
 #define ath10k_debug_get_et_stats NULL
+#define ath10k_smart_ant_debugfs_init NULL
 
 #endif /* CONFIG_ATH10K_DEBUGFS */
 #ifdef CONFIG_MAC80211_DEBUGFS

arter97/chromeos

(cherry picked from commit 48cc51589388eb2bc871e2e29a2608f31a6d1a53)
(source: wireless-3.18)
Change-Id: Ic47af77277a3f358343e57e93b7bc84ee3585fab
Signed-off-by: Ashok Raj Nagarajan <arnagara@codeaurora.org>
Reviewed-on: https://chromium-review.googlesource.com/310132
Commit-Ready: Srinivasa duvvuri <sduvvuri@chromium.org>
Tested-by: Srinivasa duvvuri <sduvvuri@chromium.org>
Reviewed-by: Srinivasa duvvuri <sduvvuri@chromium.org>

drivers/net/wireless-4.2/ath/ath10k/debug.h
@@ -37,6 +37,9 @@ enum ath10k_debug_mask {
 	ATH10K_DBG_TESTMODE	= 0x00001000,
 	ATH10K_DBG_WMI_PRINT	= 0x00002000,
 	ATH10K_DBG_PCI_PS	= 0x00004000,
+#ifdef CONFIG_ATH10K_SMART_ANTENNA
+	ATH10K_DBG_SMART_ANT	= 0x00008000,
+#endif
 	ATH10K_DBG_ANY		= 0xffffffff,
 };
 
@@ -90,6 +93,7 @@ int ath10k_debug_get_et_sset_count(struct ieee80211_hw *hw,
 void ath10k_debug_get_et_stats(struct ieee80211_hw *hw,
 			       struct ieee80211_vif *vif,
 			       struct ethtool_stats *stats, u64 *data);
+void ath10k_smart_ant_debugfs_init(struct ath10k *ar);
 #else
 static inline int ath10k_debug_start(struct ath10k *ar)
 {
@@ -145,6 +149,7 @@ ath10k_debug_get_new_fw_crash_data(struct ath10k *ar)
 #define ath10k_debug_get_et_strings NULL
 #define ath10k_debug_get_et_sset_count NULL
 #define ath10k_debug_get_et_stats NULL
+#define ath10k_smart_ant_debugfs_init NULL
 
 #endif /* CONFIG_ATH10K_DEBUGFS */
 #ifdef CONFIG_MAC80211_DEBUGFS