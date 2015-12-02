a5a1cd0fa683b96d855350039d4b0462cc739af3
Yakir Yang <ykk@rock-chips.com>
2015-12-02 17:50:56 +0800

    CHROMIUM: drm: bridge/dw_hdmi: improved the hdmi audio N/CTS cacluate math
    The original math would bring some inaccurate to N/CTS that would
    caused those magic number won't fit the HDMI 1.4 Spec request:
    	128 * SampleRate = Tmds * N / CTS;
    
    So this time we try to improved to math of N that would find the
    minimal inaccurate with the HDMI 1.4 Spec.
    
    BUG=chrome-os-partner:48022
    TEST=No more audio tone on my YAMAHA AVR
    
    Change-Id: If3f1fb5e952afa4bc02acb6dfeb07330a6ec93a6
    Signed-off-by: Yakir Yang <ykk@rock-chips.com>
    Reviewed-on: https://chromium-review.googlesource.com/315424
    Reviewed-by: Douglas Anderson <dianders@chromium.org>
    (cherry picked from commit 574c6a228d8c8697f58f99f418da67b36fa425af)
    Reviewed-on: https://chromium-review.googlesource.com/318047
    Tested-by: Douglas Anderson <dianders@chromium.org>

--- include/drm/bridge/dw_hdmi.h
+++ include/drm/bridge/dw_hdmi.h
@@ -35,6 +35,13 @@ enum dw_hdmi_devtype {
 	RK3288_HDMI,
 };
 
+struct dw_hdmi_audio_tmds_n {
+	unsigned long tmds;
+	unsigned int n_32k;
+	unsigned int n_44k1;
+	unsigned int n_48k;
+};
+
 struct dw_hdmi_mpll_config {
 	unsigned long mpixelclock;
 	struct {
@@ -82,6 +89,7 @@ struct dw_hdmi_hdcp_key_1x {
 
 struct dw_hdmi_plat_data {
 	enum dw_hdmi_devtype dev_type;
+	const struct dw_hdmi_audio_tmds_n *tmds_n_table;
 	const struct dw_hdmi_mpll_config *mpll_cfg;
 	const struct dw_hdmi_curr_ctrl *cur_ctr;
 	const struct dw_hdmi_phy_config *phy_config;
